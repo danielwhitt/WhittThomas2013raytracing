3926cfedd23de560c888544bde5b44c028912416
danielwhitt <danwhitt@gmail.com>
2021-01-08 09:39:24 -0700

    Update raytraceR.m
    more comments

--- raytraceR.m
+++ raytraceR.m
@@ -26,6 +26,25 @@ function [y_ray z_ray v_ray w_ray u_ray l_ray m_ray alphaout cgy_ray cgz_ray e_r
 % and w_ray
 
 %addpath('/Users/dwhitt/Google Drive/copymatlabstuff/matlabstuff/')
+%% COMMENTS ADDED 08 JANUARY 2021 in response to an email
+%INPUTS
+%The first four inputs (Z,dz,Y,dy) are coordinates, vertical and horizontal, e.g. vertical/depth grid Z and dz is grid spacing.
+%startz and starty are the starting coordinates of the ray to trace
+%Lz and Ly are the length of the domain to ray trace in
+%tsteps,dt are timestepping parameters for the ray tracing (number of steps, time step in seconds)
+%thresh defines a threshold (small) vertical group velocity at which to internally reflect
+%m0 is an initial vertical wavenumber of the wave packet (the vertical wavenumber evolves evolves through the ray tracing)
+%omega is the intrinsic frequency of the wave packet (the frequency remains fixed through the ray tracing)
+%F2,S2,N2 ("F squared, S squared and N squared" are as defined in Whitt and Thomas 2013)
+%v0 is an initial wave velocity amplitude (e.g. 0.1 m/s), perpendicular to the mean current u (this evolves through the ray tracing)
+%f is the coriolis frequency
+%chstart (1 or 2) indicates which of two characteristics the initial wave packet is on; see Whitt and Thomas 2013
+%s_M (=F2/S2) is the slope of absolute geostrophic momentum surfaces Mg=ug-fy
+%ug is the background geostrophic flow field (function of y,z)
+%various second derivatives of the background geostrophic velocity and buoyancy field: d2udy2,d2udz2,d2bdz2,d2bdy2,d2bdzdy,d2udzdy; these get set to zero in the test case.
+
+%%
+
 
 % initialize flags
 reflectflag = 0;
